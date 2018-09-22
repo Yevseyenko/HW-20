1d7a7c97ba95b4cfd32ca2b2e7ba033544612d53
Yevseyenko <jevsejen@gmail.com>
2018-09-23 00:30:56 +0300

    Adding assert

--- src/test/java/com/epam/TestSendingEmail.java
+++ src/test/java/com/epam/TestSendingEmail.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -43,6 +44,7 @@ public class TestSendingEmail {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        Assert.assertTrue(driver.findElement(By.cssSelector("span.bAq")).isDisplayed());
         driver.quit();
     }
 
